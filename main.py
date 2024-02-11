ac29e876475e5b639cb6aaebd2a46ec6e68a959a
AlebgDev <bryanalejandro3107@gmail.com>
2024-02-11 02:21:09 -0400

    FastAPI - Crud

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from typing import Union,Text
+from typing import Union,Text,Optional
 from pydantic import BaseModel
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from datetime import datetime
+from uuid import uuid4 as uuid
 
 app = FastAPI()
 
@@ -9,19 +10,50 @@ posts = []
 
 #Post model
 class Post(BaseModel):
-    id: str
+    id: Optional[str]
     title: str
     author: str
     content: Text 
     created_at: datetime=datetime.now()
-    published_at: datetime
+    published_at: Optional[datetime]
+    published: bool = False
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/posts")
+def get_posts():
+    return posts
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/posts")
+def save_posts(post: Post):
+    post.id = str(uuid())
+    posts.append(post.dict())
+    return posts[-1]
+
+@app.get("/posts/{post_id}")
+def get_post(post_id : str):
+    for post in posts:
+        if post["id"] == post_id:
+            return post
+    raise HTTPException(status_code=404, detail="Post not found")
+
+@app.delete("/posts/{post_id}")
+def delete_post(post_id: str):
+    for index,post in enumerate(posts):
+        if post["id"] == post_id:
+            posts.pop(index)
+            return {"Message": "Post has been deleted"}
+    raise HTTPException(status_code=404, detail="Post not found")
+
+@app.put('/posts/{post_id}')
+def update_post(post_id: str, updatedPost: Post):
+    for index, post in enumerate(posts):
+        if post["id"] == post_id:
+            posts[index]["title"]= updatedPost.dict()["title"]
+            posts[index]["content"]= updatedPost.dict()["content"]
+            posts[index]["author"]= updatedPost.dict()["author"]
+            return {"message": "Post has been updated succesfully"}
+    raise HTTPException(status_code=404, detail="Item not found")
